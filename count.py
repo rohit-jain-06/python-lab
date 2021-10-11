bdfdd888128748654025b9031ec537207b487b59
rohit-jain-06 <83217987+rohit-jain-06@users.noreply.github.com>
2021-10-12 00:06:46 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,7 @@
+# Q. Write a program to count the number of zeroes in following tuple.  a = (7,0,8,0,0,9)
+a = (7,0,8,0,0,9)
+	print("Number of Zeroes in tuple is: ", a.count(0))
+	
+
+	# Output : Number of Zeroes in tuple is:  3
+
