2e9f3ac8303a9c95dd7eff16bcb442795c93abb5
rohit-jain-06 <83217987+rohit-jain-06@users.noreply.github.com>
2021-10-12 00:36:39 +0530

    Create uniqueno.py

---
+++ uniqueno.py
@@ -0,0 +1,19 @@
+# Q. Write a program to input 8 numbers from the user and display all the unique numbers (once).
+
+A = []
+print("Enter the Element of List :")
+for i in range(8):
+    A.append(input(""))
+
+print("The unique values from the List is :", set(A))
+
+# Output : Enter the Element of List :
+#             1
+#             2
+#             3
+#             5
+#             2
+#             3 
+#             4
+#             6
+#             The unique values from the List is : {'2', '3', '5', '1', '6', '4'}
